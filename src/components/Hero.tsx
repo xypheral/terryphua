e067d8c7e69548f6ae629003004267887809ff08
Wei Han <noelng26@gmail.com>
2024-01-02 12:29:33 +0800

    updates

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,25 +5,24 @@ import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
-    <section className="flex flex-col-reverse md:flex-row items-center justify-center p-10 md:p-20 mt-20 mb-10">
-      <div className="md:w-3/5 space-y-5 md:mr-4">
+    <div className="flex flex-col-reverse md:flex-row items-center justify-center p-10 md:p-20 h-screen">
+      <div className="md:w-3/5 space-y-5 md:mr-4 text-center md:text-left">
         <h1 className="text-5xl font-bold text-gray-800">Hello, I'm Terry Phua</h1>
         <p className="text-2xl text-gray-800"></p>
       </div>
       <motion.div
-  className="w-full md:w-2/5 mt-10 md:mt-0"
-  initial={{ scale: 0 }}
-  animate={{ rotate: 360, scale: 1 }}
-  transition={{ 
-      type: "spring",
-      stiffness: 260,
-      damping: 20
-  }}
->
-  <Image src={heroImage} alt="John Doe" layout="responsive" width={640} height={480} className="w-full h-auto md:h-400" />
-</motion.div>
-    </section>
+        className="w-full md:w-2/5 mt-10 md:mt-0"
+        initial={{ scale: 0 }}
+        animate={{ rotate: 360, scale: 1 }}
+        transition={{ 
+            type: "spring",
+            stiffness: 260,
+            damping: 20
+        }}
+      >
+        <Image src={heroImage} alt="John Doe" layout="responsive" width={640} height={480} className="w-full h-auto md:h-400" />
+      </motion.div>
+    </div>
   );
 };
-
 export default Hero;
