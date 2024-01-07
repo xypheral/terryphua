59e3fcf8d1e7481019df0deb8e3f96b5f994b9f3
Wei Han <87291797+xypheral@users.noreply.github.com>
2024-01-07 21:22:56 +0800

    updates

---
+++ src/components/alerts/Alert.tsx
@@ -0,0 +1,24 @@
+import { ExclamationTriangleIcon } from '@heroicons/react/20/solid'
+
+const Alert = () => {
+  return (
+    <div className="rounded-md bg-red-50 p-4 mb-5">
+      <div className="flex">
+        <div className="flex-shrink-0">
+          <ExclamationTriangleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+        </div>
+        <div className="ml-3">
+          <h3 className="text-sm font-medium text-yellow-800">Attention needed</h3>
+          <div className="mt-2 text-sm text-red-700">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo
+              totam eius aperiam dolorum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Alert
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,11 @@
 import RootLayout from "@/app/layout"
+import Alert from "@/components/alerts/Alert"
 
 export default function About() {
   return (
     <RootLayout title="About">
     <div className="p-20 mt-10">
+      <Alert />
       <div className="px-4 sm:px-0">
         <h3 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h3>
         <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details and application.</p>
