933c3dde6f113eb6aca2e711467f367fad165926
Wei Han <noelng26@gmail.com>
2024-01-08 11:38:21 +0800

    updates

--- src/components/alerts/Alert.tsx
+++ src/components/alerts/Alert.tsx
@@ -2,7 +2,7 @@ import { ExclamationTriangleIcon } from '@heroicons/react/20/solid'
 
 const Alert = () => {
   return (
-    <div className="rounded-md bg-red-50 p-4 mb-5">
+    <div className="rounded-md bg-red-100 p-4 mb-5">
       <div className="flex">
         <div className="flex-shrink-0">
           <ExclamationTriangleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
