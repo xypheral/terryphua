03fbdc3546ccf92baadeed5b84b78b24c2b003df
Wei Han <87291797+xypheral@users.noreply.github.com>
2024-01-13 21:00:03 +0800

    updates

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -2,26 +2,31 @@ const Landing = () => {
     return (
         <div className="relative bg-white h-screen flex flex-col justify-between">
 
-            <div className="absolute left-6 top-36 bottom-6 w-1 bg-gradient-to-b from-sky-400 to-purple-600">
-                <p className="text-sm transform -rotate-90 absolute top-[-50px] left-[-21px] whitespace-nowrap text-sky-500">Design</p>
-                {/* <p className="text-sm transform -rotate-90 absolute top-[-50px] left-[-21px] whitespace-nowrap bg-clip-text text-transparent bg-gradient-to-r from-cyan-400 to-blue-500">Design</p> */}
+            <div className="absolute left-6 top-36 bottom-6 w-1 bg-gradient-to-b from-pink-400 to-violet-600">
+                <p className="text-lg font-bold transform -rotate-90 absolute top-[-50px] left-[-20px] whitespace-nowrap bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-800">TPCH</p>
             </div>
 
-            <div className="absolute right-6 top-6 bottom-36 w-1 translate-y-14 bg-gradient-to-b from-purple-600 to-sky-400">
-                <p className="text-sm transform -rotate-90 absolute bottom-[-50px] right-[-21px] whitespace-nowrap text-sky-500">Design</p>
+            <div className="absolute right-6 top-6 bottom-36 w-1 translate-y-14 bg-gradient-to-b from-violet-600 to-pink-400">
+                <p className="text-lg font-bold transform rotate-90 absolute bottom-[-50px] right-[-21px] whitespace-nowrap  bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-800">TPCH</p>
             </div>
 
             <div className="w-full h-24 mt-20 flex justify-between">
-                <p className="text-2xl mt-6 ml-16 font-semibold bg-clip-text text-transparent bg-gradient-to-r from-sky-800 to-sky-400">哈哈哈哈哈</p>
-                <p className="text-2xl mt-6 mr-16 font-semibold bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-purple-800">测试 001</p>
+                <p className="text-2xl mt-3 ml-16 font-bold bg-clip-text text-transparent bg-gradient-to-r from-violet-800 to-violet-400">TERRY</p>
             </div>
 
-            <div className="w-full h-100 mb-8 flex justify-between">
-                <p className="text-6xl ml-16 font-bold text-black translate-x-10">当你在读这短句，<br /> 你就是个大傻逼。</p>
+            <div className="w-full h-100 mb-24 flex justify-center">
+                <p className="text-6xl ml-16 font-bold text-black">
+                    <span className="block">Welcome to</span>
+                    <span className="inline">my </span>
+                    <span className="relative inline-block">
+                    Portfolio
+                    <span className="absolute h-[0.08em] bottom-0 inset-x-0 bg-gradient-to-r from-violet-500 to-pink-500"></span>
+                    </span>
+                </p>
             </div>
 
-            <div className="w-full h-100 mb-8 flex justify-between">
-                <p className="text-2xl ml-16 bg-clip-text text-transparent bg-gradient-to-r from-purple-600 to-sky-500">恍恍惚惚就像掉进了那无底洞</p>
+            <div className="w-full h-100 mb-8 flex justify-end">
+                <p className="text-2xl mt-3 mr-16 font-bold bg-clip-text text-transparent bg-gradient-to-r from-violet-400 to-violet-800">PHUA</p>
             </div>
 
         </div>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,7 @@ import Hero from '@/components/Hero';
 import Middle from '@/components/Middle';
 import Footer from '@/components/Footer';
 import Landing from '@/components/Landing';
+
 export default function Home() {
   return (
     <main className="flex flex-col justify-center">
